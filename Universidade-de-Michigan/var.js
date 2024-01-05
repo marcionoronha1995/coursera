05763f133ae96dd0420f0af31bd4b97f4789fca9
marcionoronha1995 <marcionoronha1995@gmail.com>
2024-01-05 01:52:57 -0300

    var function()

---
+++ Universidade-de-Michigan/var.js
@@ -0,0 +1,5 @@
+var funcM = function(nome, par, letra){ var nome; var par; var letra; nome = par + letra};
+var funcMConsole = function(){console.log("teste")}
+var funcMConsoleL = () => {
+    console.log("teste na func M Console L");
+}
