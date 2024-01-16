24ae4844034fff67f2af85ee5c55481f2b0a1976
marcionoronha1995 <marcionoronha1995@gmail.com>
2024-01-16 02:41:57 -0300

    calcl innerHTML

--- Universidade-de-Michigan/outPut/variaveis.js
+++ Universidade-de-Michigan/outPut/variaveis.js
@@ -1,1 +1,25 @@
 console.log("variaveis js file");
+// Numbers:
+let lengthh = 16;
+let weight = 7.5;
+document.getElementById("var1").innerHTML = lengthh;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+document.getElementById("var2").innerHTML = color;
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+console.log("fim do variaveis.js");
+
