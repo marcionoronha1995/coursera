3e37b1557d52d026f8d9950530d8cea46a08211b
marcionoronha1995 <marcionoronha1995@gmail.com>
2024-01-17 08:52:23 -0300

    html dom function

--- Universidade-de-Michigan/outPut/variaveis.js
+++ Universidade-de-Michigan/outPut/variaveis.js
@@ -2,12 +2,20 @@ console.log("variaveis js file");
 // Numbers:
 let lengthh = 16;
 let weight = 7.5;
-document.getElementById("var1").innerHTML = lengthh;
-
-// Strings:
+    // Strings:
 let color = "Yellow";
 let lastName = "Johnson";
-document.getElementById("var2").innerHTML = color;
+function variaveis(){
+    console.log("dentro de variaveis");
+    console.log(lengthh)
+    document.getElementById("var1").innerHTML = lengthh;
+    document.getElementById("var2").innerHTML = color;
+    document.getElementById("var3").innerHTML = color.length;
+    document.getElementById("var4").innerHTML = lastName.length;
+    document.querySelector("p").style.backgroundColor = "red";
+    document.querySelector("a").style.backgroundColor = "yellow";
+        
+};
 
 // Booleans
 let x = true;
@@ -22,4 +30,14 @@ const cars = ["Saab", "Volvo", "BMW"];
 // Date object:
 const date = new Date("2022-03-25");
 console.log("fim do variaveis.js");
+
+//Html DOM
+function htmlDOM(){
+    document.getElementById("var1").style.backgroundColor = "green";
+    document.getElementById("var3").style.backgroundColor = "green";
+    document.getElementById("var5").style.backgroundColor = "green";
+    document.getElementById("var7").style.backgroundColor = "green";
+}
+
+
 
